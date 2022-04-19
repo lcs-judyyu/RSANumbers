739e5919a8e344cce3cc16cbefc7dcb692370470
Judy <jyu@lcs.on.ca>
2022-04-19 09:27:33 -0400

    Initial Commit

---
+++ RSANumbers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RSANumbers
+//
+//  Created by Judy Yu on 2022-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RSANumbers/RSANumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSANumbersApp.swift
+//  RSANumbers
+//
+//  Created by Judy Yu on 2022-04-19.
+//
+
+import SwiftUI
+
+@main
+struct RSANumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
